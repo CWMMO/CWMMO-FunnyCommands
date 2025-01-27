7a71349138006e5d932fa47386cd97e50264e243
CWMMO <alanpenguino@gmail.com>
2025-01-26 19:37:07 -0500

    Update CWMMOFunnyCommands.java

--- src/main/java/com/cwmmo/cWMMOFunnyCommands/CWMMOFunnyCommands.java
+++ src/main/java/com/cwmmo/cWMMOFunnyCommands/CWMMOFunnyCommands.java
@@ -1,13 +1,18 @@
 package com.cwmmo.cWMMOFunnyCommands;
 
+import com.cwmmo.cWMMOFunnyCommands.Commands.KarmaCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CWMMOFunnyCommands extends JavaPlugin {
+    public static CWMMOFunnyCommands INSTANCE;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        INSTANCE = this;
 
+        saveDefaultConfig();
+
+        getCommand("karma").setExecutor(new KarmaCommand());
     }
 
 }
