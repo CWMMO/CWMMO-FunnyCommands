861c839a771368721227e7d7ae0365b5c1bf4ea4
CWMMO <alanpenguino@gmail.com>
2025-01-26 19:37:22 -0500

    Update ConfigManager.java

--- src/main/java/com/cwmmo/cWMMOFunnyCommands/Utils/ConfigManager.java
+++ src/main/java/com/cwmmo/cWMMOFunnyCommands/Utils/ConfigManager.java
@@ -1,4 +1,36 @@
 package com.cwmmo.cWMMOFunnyCommands.Utils;
 
+import com.cwmmo.cWMMOFunnyCommands.CWMMOFunnyCommands;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
+import java.util.Objects;
+
 public class ConfigManager {
+    public final FileConfiguration CONFIG =
+            CWMMOFunnyCommands.INSTANCE.getConfig();
+
+    public Component getLocale(String key) {
+        return MiniMessage.miniMessage().deserialize(
+                Objects.requireNonNull(CONFIG.getString(key))
+        );
+    }
+    public Component getLocale(String key, Object... formatted) {
+        return MiniMessage.miniMessage().deserialize(
+                Objects.requireNonNull(CONFIG.getString(key).formatted(formatted))
+        );
+    }
+
+    public String getString(String key) {
+        return CONFIG.getString(key);
+    }
+    public String getString(String key, Object... formatted) {
+        return CONFIG.getString(key).formatted(formatted);
+    }
+
+    public List<String> getStringList(String key) {
+        return CONFIG.getStringList(key);
+    }
 }
